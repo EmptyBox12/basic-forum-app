65c32a99ca9a9a16f139fe0bd46594500256377f
EmptyBox12 <ataberktumay@gmail.com>
2021-12-20 00:14:56 +0300

    add delete comment

--- api/controllers/commentController.js
+++ api/controllers/commentController.js
@@ -16,3 +16,28 @@ exports.createComment = async (req, res) => {
     res.status(400).json({ status: "fail", msg: "Can't create comment", e });
   }
 };
+exports.deleteComment = async (req, res) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+    const post = await Post.findById(comment.post);
+    if (
+      req.user.newUser._id != comment.commentor ||
+      req.user.newUser._id != post.user
+    ) {
+      res
+        .status(400)
+        .json({
+          status: "fail",
+          msg: "You don't have permission to delete this comment",
+          e,
+        });
+    }
+    const index = post.comments.indexOf(comment._id);
+    post.comments.splice(index, 1);
+    await post.save();
+    await Comment.deleteOne({ _id: comment._id });
+    res.status(200).json({ status: "success", msg: "comment deleted" });
+  } catch (e) {
+    res.status(400).json({ status: "fail", msg: "Can't delete comment", e });
+  }
+};
--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -6,5 +6,6 @@ const commentController = require("../controllers/commentController");
 const router = express.Router();
 //post slug
 router.route("/:slug").post(authMiddleware, commentController.createComment);
+router.route("/:id").delete(authMiddleware, commentController.deleteComment);
 
 module.exports = router;
